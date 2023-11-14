3df7b91529ba17817c98c9bce3cdf9162e4eb515
Ilyasfirdaus <95671064+Ilyasfirdaus@users.noreply.github.com>
2023-11-14 21:57:51 +0700

    Update beranda.php

--- application/views/beranda.php
+++ application/views/beranda.php
@@ -13,5 +13,47 @@
   
   </div>
   
+
+        <div class="container">
+            <div class="wrapper mt-4">
+                <form method="post" class="d-flex" action="<?php echo site_url('Welcome/pencarianbuku'); ?>">
+                    <input type="text" name="cari" placeholder="Judul Buku" class="form-control me-2">
+                    <button class="btn btn-outline-primary" type="submit">Search</button>
+                </form>
+                </form>
+                <table class="table table-bordered table-hover mt-2">
+                    <thead>
+                        <tr>
+                            <th>No. Buku</th>
+                            <th>Nama Buku</th>
+                            <th>Kategori</th>
+                            <th>Harga</th>
+                            <th>Stok</th>
+                            <th>Penerbit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php if (isset($GetDataCariBuku) && !empty($GetDataCariBuku)) {
+                            foreach ($GetDataCariBuku as $buku) { ?>
+                                <tr>
+                                    <td><?php echo $buku->id_buku; ?></td>
+                                    <td><?php echo $buku->nama_buku; ?></td>
+                                    <td><?php echo $buku->kategori; ?></td>
+                                    <td><?php echo $buku->harga; ?></td>
+                                    <td><?php echo $buku->stok; ?></td>
+                                    <td><?php echo $buku->penerbit; ?></td>
+                                </tr>
+                            <?php
+                            }
+                        } else { ?>
+                            <tr>
+                                <td colspan="2">Tidak ada hasil pencarian.</td>
+                            </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
 </div>
 <!-- akhir -->
+
